a58acde0af59a7af01de7883ef46c7236c597db9
kubaobara <siemagosciu32@gmail.com>
2023-06-15 15:24:47 +0200

    joined client to gui

--- src/main/java/com/example/gui_client/HelloApplication.java
+++ src/main/java/com/example/gui_client/HelloApplication.java
@@ -10,8 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        ServerThread serverThread = new ServerThread("localhost",5000);
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/gui_client/HelloController.java
+++ src/main/java/com/example/gui_client/HelloController.java
@@ -1,14 +1,13 @@
 package com.example.gui_client;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    public TextField inputField;
+    public Button sendButton;
+    public Button sendFileButton;
+    public ListView clientList;
+    public ProgressBar fileProgressBar;
 }
